c085663d96e927a90982cd8f4e6f15a62c45e5ca
Jin Choo <jinchoo315@gmail.com>
2021-01-08 01:22:51 -0600

    need to fix index.js, stll need to add ReadMe, and deply to heroku.  Fixed issue with .DS_Store.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,161 +1,40 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then((reg) => {
-      console.log("Service worker registered.", reg);
-    });
-  });
-}
-
 let transactions = [];
 let myChart;
-const transactionForm = createTransactionForm();
-const transactionApi = createTransactionApi();
 
-initTransactions();
-
-document.querySelector("#add-btn").onclick = function () {
-  sendTransaction(true);
-};
-
-document.querySelector("#sub-btn").onclick = function () {
-  sendTransaction(false);
-};
-
-function createTransactionForm() {
-  const nameEl = document.querySelector("#t-name");
-  const amountEl = document.querySelector("#t-amount");
-  const errorEl = document.querySelector(".form .error");
-
-  const showError = (message) => {
-    errorEl.textContent = message;
-  };
-
-  // return false if invalid and display validation message
-  const validate = () => {
-    // validate form
-    if (nameEl.value === "" || amountEl.value === "") {
-      showError("Missing Information");
-      return false;
-    }
-    showError("");
-    return true;
-  };
-
-  // return transaction object from form input
-  const transaction = () => {
-    return {
-      name: nameEl.value,
-      value: amountEl.value,
-      date: new Date().toISOString(),
-    };
-  };
-
-  // clear form inputs
-  const clear = () => {
-    nameEl.value = "";
-    amountEl.value = "";
-    showError("");
-  };
-
-  return Object.freeze({ transaction, validate, clear, showError });
-}
-
-function createTransactionApi() {
-  const create = (transaction) => {
-    return fetch("/api/transaction", {
-      method: "POST",
-      body: JSON.stringify(transaction),
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      return response.json();
-    });
-  };
-
-  const fetchAll = () => {
-    return fetch("/api/transaction").then((response) => {
-      return response.json();
-    });
-  };
-  return Object.freeze({ create, fetchAll });
-}
-
-function initTransactions() {
-  transactionApi.fetchAll().then((data) => {
+fetch("/api/transaction")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
     // save db data on global variable
     transactions = data;
 
-    renderTransactionsChart();
+    populateTotal();
+    populateTable();
+    populateChart();
   });
-}
-
-function sendTransaction(isAdding) {
-  if (!transactionForm.validate()) {
-    return;
-  }
-
-  // create record
-  const transaction = transactionForm.transaction();
-
-  // if subtracting funds, convert amount to negative number
-  if (!isAdding) {
-    transaction.value *= -1;
-  }
-
-  // add to beginning of current array of data
-  transactions.unshift(transaction);
-
-  // re-run logic to populate ui with new record
-  populateChart();
-  populateTable();
-  populateTotal();
-
-  // also send to server
-  transactionApi
-    .create(transaction)
-    .then((data) => {
-      if (data.errors) {
-        transactionForm.showError("Missing Information");
-      } else {
-        transactionForm.clear();
-      }
-    })
-    .catch(() => {
-      // fetch failed, so save in indexed db
-      saveRecord(transaction);
-      transactionForm.clear();
-    });
-}
-
-function renderTransactionsChart() {
-  populateTotal();
-  populateTable();
-  populateChart();
-}
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  const total = transactions.reduce((total, t) => {
+  let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
 
-  const totalEl = document.querySelector("#total");
+  let totalEl = document.querySelector("#total");
   totalEl.textContent = total;
 }
 
 function populateTable() {
-  const tbody = document.querySelector("#tbody");
+  let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
   transactions.forEach((transaction) => {
     // create and populate a table row
-    const tr = document.createElement("tr");
+    let tr = document.createElement("tr");
     tr.innerHTML = `
-        <td>${transaction.name}</td>
-        <td>${transaction.value}</td>
-      `;
+      <td>${transaction.name}</td>
+      <td>${transaction.value}</td>
+    `;
 
     tbody.appendChild(tr);
   });
@@ -163,17 +42,17 @@ function populateTable() {
 
 function populateChart() {
   // copy array and reverse it
-  const reversed = transactions.slice().reverse();
+  let reversed = transactions.slice().reverse();
   let sum = 0;
 
   // create date labels for chart
-  const labels = reversed.map((t) => {
-    const date = new Date(t.date);
+  let labels = reversed.map((t) => {
+    let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
   // create incremental values for chart
-  const data = reversed.map((t) => {
+  let data = reversed.map((t) => {
     sum += parseInt(t.value);
     return sum;
   });
@@ -183,7 +62,7 @@ function populateChart() {
     myChart.destroy();
   }
 
-  const ctx = document.getElementById("myChart").getContext("2d");
+  let ctx = document.getElementById("myChart").getContext("2d");
 
   myChart = new Chart(ctx, {
     type: "line",
@@ -200,3 +79,75 @@ function populateChart() {
     },
   });
 }
+
+function sendTransaction(isAdding) {
+  let nameEl = document.querySelector("#t-name");
+  let amountEl = document.querySelector("#t-amount");
+  let errorEl = document.querySelector(".form .error");
+
+  // validate form
+  if (nameEl.value === "" || amountEl.value === "") {
+    errorEl.textContent = "Missing Information";
+    return;
+  } else {
+    errorEl.textContent = "";
+  }
+
+  // create record
+  let transaction = {
+    name: nameEl.value,
+    value: amountEl.value,
+    date: new Date().toISOString(),
+  };
+
+  // if subtracting funds, convert amount to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
+
+  // add to beginning of current array of data
+  transactions.unshift(transaction);
+
+  // re-run logic to populate ui with new record
+  populateChart();
+  populateTable();
+  populateTotal();
+
+  // also send to server
+  fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      } else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch((err) => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    });
+}
+
+document.querySelector("#add-btn").onclick = function () {
+  sendTransaction(true);
+};
+
+document.querySelector("#sub-btn").onclick = function () {
+  sendTransaction(false);
+};
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,24 @@
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v2";
+
+const iconSizes = ["72", "96", "128", "144", "152", "192", "384"];
+const iconFiles = iconSizes.map(
+  (size) => `/assets/images/icons/icon-${size}x${size}.png`
+);
+
+
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/assets/css/styles.css",
   "/assets/js/index.js",
   "/assets/js/db.js",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/manifest.webmanifest",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-];
+].concat(iconFiles);
 
-const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
